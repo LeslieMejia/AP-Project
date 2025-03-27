22688062b5f41dd1e2f829f2b4860592330d704a
Jasmin Labrosse <jasminlabrosse@MacBook-Air-tilhrende-Jasmin.local>
2025-03-27 11:29:30 +0100

    I added jobseeker login

---
+++ CourseAdminSystemAngular/src/app/jobseeker-signup/jobseeker-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobseeker-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './jobseeker-signup.component.html',
+  styleUrl: './jobseeker-signup.component.css'
+})
+export class JobseekerSignupComponent {
+
+}
