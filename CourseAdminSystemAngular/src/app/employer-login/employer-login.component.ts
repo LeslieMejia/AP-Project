ffe00935807fab3beec75853d9528073cec6cc32
LeslieMejia <leme24ab@student.cbs.dk>
2025-03-27 12:35:52 +0100

    employer login c

---
+++ CourseAdminSystemAngular/src/app/employer-login/employer-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-login.component.html',
+  styleUrl: './employer-login.component.css'
+})
+export class EmployerLoginComponent {
+
+}
