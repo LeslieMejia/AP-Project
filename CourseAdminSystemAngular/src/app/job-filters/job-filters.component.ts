414f2e410ba1b440ba8f7f3c4242ca6f71273e97
Mathilde Ellemose Andersen <mathildeellemoseandersen@users-MacBook-Pro.local>
2025-03-27 12:17:31 +0100

    Add job-filters component

---
+++ CourseAdminSystemAngular/src/app/job-filters/job-filters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-filters.component.html',
+  styleUrl: './job-filters.component.css'
+})
+export class JobFiltersComponent {
+
+}
